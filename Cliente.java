cbcf0d865f042f4d5e92f4b5a3ff3b1753fb83e8
FAS <fernando.barrera@unmsm.edu.pe>
2024-10-18 18:41:54 -0500

    project

---
+++ Cliente.java
@@ -0,0 +1,19 @@
+package Proyecto;
+
+public class Cliente {
+    private String nombresCliente;
+    private String apellidoPaternoCliente;
+    private String apellidoMaternoCliente;
+    private String sexoCliente;
+    private String dniCliente;
+    private String telefonoCliente;
+    private String correoCliente;
+    private String ocupacionCliente;
+    private String profesionCliente;
+    private String estadoCivilCliente;
+    private String cargaFamiliarCliente;
+    private String direccionCliente;
+    private String codigoUbigeoCliente;
+    private String cuentaCliente;
+    private String liquidezFinanciera;
+}
---
+++ Sucursal.java
@@ -0,0 +1,9 @@
+package Proyecto;
+
+public class Sucursal {
+    private String direccionSucursal;
+    private String codigoSucursal;
+    private String ciudadSurcursal;
+    private int cantidadEmpleados;
+    private Empleado[] conjuntoEmpleados;
+}
