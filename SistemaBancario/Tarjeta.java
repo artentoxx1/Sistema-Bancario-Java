850e18980ba5afc3bf2cd9aa3ed9f4b25cc71ce9
FAS <fernando.barrera@unmsm.edu.pe>
2024-10-27 21:45:12 -0500

    Añadir

--- SistemaBancario/Tarjeta.java
+++ SistemaBancario/Tarjeta.java
@@ -9,4 +9,10 @@ public class Tarjeta {
         this.claveTarjeta = claveTarjeta;
         this.cuentaVinculada = null;
     }
+    public static String generarNumeroTarjeta() {
+        return String.format("%016d", (long) (Math.random() * 1_000_000_000_000_000L));
+    }
+    public static String generarClaveTarjeta() {
+        return String.format("%04d", (int) (Math.random() * 10000));
+    }
 }
